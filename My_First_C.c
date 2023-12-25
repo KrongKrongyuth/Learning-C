fc8e82dafd5584e33548b8d15ddef6f99e3569e4
TON_KKrongyuth <ton.kkrongyuth@gmail.com>
2023-12-25 13:22:02 +0700

    commit Lecture 2

--- My_First_C.c
+++ My_First_C.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main(){
+    // This is a single line comment
+    /*  
+        This
+        is 
+        a 
+        multiline
+        comment
+    */
     printf("Hello World\n");
-    printf("This is my First Program in C\n");
+    printf("\nThis\nis\nmy\nFirst\nProgram\nin\nC\n");
+    printf("\n1\t2\t3\t\n4\t5\t6\t\n7\t8\t9\n"); // \t -> 1 tab, \n -> 1 enter
+    printf("\n\"Some thing hard to belive\" -- add (\"...\") in to statemnt\n"); // We can add ("") or (\) into our statemnt by using \"\", \\ respectively.
     return 0;
-}
+    }
