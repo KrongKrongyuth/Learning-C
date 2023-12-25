2b9305373943bf3472c471c16e3931990f23c6a3
TON_KKrongyuth <ton.kkrongyuth@gmail.com>
2023-12-25 15:02:24 +0700

    commit Lecture 6 : Constants

---
+++ Lecture_6_Constants.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    // cinstant = fixed value that cannot be altered by the program during its execution.
+
+    const float PI = 3.14159; // We cannot edit the constant variable.
+
+    // PI = 555; // Error
+
+    printf("\n%f\n",PI);
+
+    return 0;
+}
